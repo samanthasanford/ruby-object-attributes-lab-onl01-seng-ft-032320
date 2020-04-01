ab9d1e90ce0930aafb7d72fe4a7ad06597b80ed5
Samantha Sanford <samanthawebdev@gmail.com>
2020-04-01 00:31:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,8 +6,13 @@ class Dog
   def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
+
+  def breed
+    @breed
+  end
+
+  def breed=(dogs_breed)
+  end
 end
 
-fido = Dog.new
-fido.name = "Fido"
-fido
+
